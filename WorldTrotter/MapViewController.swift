5bfc5d66532059efc3ef9604cb81a311e7dfa761
Mark Engen <markengen@marks-air>
2017-09-11 20:01:59 -0400

    App now has 3 views (Conversion, Map and Web).  Working, but the constraints need to be tweaked to fit nicely in landscape and portrait modes on any device.  Currently only working on iPhone7 Portrait mode.

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,68 @@
 //
 
 import UIKit
+import MapKit
+
 
 class MapViewController: UIViewController {
+
+    var mapView: MKMapView!  //declare a variable of MKMapView type
+    //loadView is called when a view's view property is nil (before we were creating views via the GUI and linking is called where the view doesn't have (i.e. is nil) 
+    override func loadView() {
+        //Create a map view outside the GUI dragging/dropping interface
+        mapView = MKMapView()  //Create an instance of MKMapView in mapView
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
+        segmentedControl.selectedSegmentIndex = 0
+        
+        
+        //Page 108 explains what the next line does in the iOS Programming book
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        //segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        segmentedControl.addTarget(self,
+                                   action: #selector(MapViewController.mapTypeChanged(_:)),
+                                   for: .valueChanged)
+        
+        view.addSubview(segmentedControl)
+        //We now have a view, but nobody will ever see it until we do something else.
+        
+        //Let's create some layout contraint instances...
+        //Align the Standard, Hybrid and Satellite buttons with the top of the parent view
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -50)
+        let bottomConstraint = segmentedControl.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8)
+        bottomConstraint.isActive = true
+        //Align the Standard, Hybrid and Satellite buttons with the left edge of the parent view
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8)
+        //Align the Standard, Hybrid and Satellite buttons with the right edge of the parent view
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
+        
+        //Now we activate the constraint instances defined above using the isActive property
+        //See page 110 in iOS Programming for further info on the below commands and what they affect in the view hierarchy
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        //let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        topConstraint.isActive = true
+
+        
+    }
+    
+    func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
 
     /* viewWillAppear will execute just before, and each time the view is called to load.  Executes after viewDidLoad */
     override func viewWillAppear(_ animated: Bool) {
---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Mark Engen on 9/11/17.
+//  Copyright © 2017 Mark Engen. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var webView: UIWebView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //let url = NSURL(string: "http://www.google.com")
+        //let request = NSURLRequest(URL: url!)
+        let electronicArmoryURL = URL(string: "http://www.topprice4yourhome.com/message2-working.php")
+        let electronicArmoryURLRequest = URLRequest(url: electronicArmoryURL!)
+        webView.loadRequest(electronicArmoryURLRequest)
+        print("Got here")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+}
