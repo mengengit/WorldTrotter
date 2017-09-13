2ea5b0b4f5fc04fe596307ae8f4c620c963c1181
Mark Engen <markengen@marks-air>
2017-09-13 00:07:55 -0400

    Tab bar icons are blue when selected and gray when not.  Tab bar captions/labels are black when selected and gray when not.  Everything working including changing the colors of the text as specified in the view controller for the web view.

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -21,6 +21,19 @@ class ViewController: UIViewController {
         let electronicArmoryURLRequest = URLRequest(url: electronicArmoryURL!)
         webView.loadRequest(electronicArmoryURLRequest)
         print("Got here")
+        
+        /* changing text colors in tab bar courtesy of https://stackoverflow.com/questions/31117069/changing-tab-bar-item-image-and-text-color-ios */
+        
+        let selectedColor   = UIColor(red: 180.0/255.0, green: 180.0/255.0, blue: 180.0/255.0, alpha: 3.0)
+        let unselectedColor = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 3.0)
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: selectedColor], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: unselectedColor], for: .selected)
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Map View has focus")
     }
     
     override func didReceiveMemoryWarning() {
